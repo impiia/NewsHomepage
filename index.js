eb508889ccc37fe615df1bbb8d1ae719906b7961
impiia <impiia28@gmail.com>
2023-05-10 01:12:37 +0400

    add header background during scrolling

--- index.js
+++ index.js
@@ -12,3 +12,15 @@ menuCloseItem.addEventListener('click', ()=>{
     menu.classList.remove('header__nav_active');
 })
 }());
+
+(function(){
+const header =document.querySelector('.header');
+window.onscroll = ()=>{
+    if (window.pageYOffset>50){
+        header.classList.add('header_active');
+    }else{
+        header.classList.remove('header_active');
+    }
+};
+
+}());
